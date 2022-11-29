639939379ed4d08bc232229da28ab92d7aaaa297
Sergey Kolesan <111977948+Kolesanencore@users.noreply.github.com>
2022-11-29 23:05:29 +0200

    Revert "Footer modal"

--- src/index.js
+++ src/index.js
@@ -9,12 +9,3 @@ import './js/components/modal.js';
 import './js/components/trailers.js';
 
 import './js/servises/header';
-
-import './js/modal_footer';
-import './js/render_footer_modal';
-import './js/slider_glide';
-
-
-
-
-
