6893f76e217549887c0e9c45da1317f2aa79cb4c
Kolesanencore <kolesanencore@gmail.com>
2022-11-29 20:18:42 +0200

    Revert "Footer modal"
    This reverts commit a2ab712dab000477560289439a5445b268b9f390.

--- src/index.js
+++ src/index.js
@@ -8,15 +8,3 @@ import './js/modal'
 
 import './js/servises/header';
 
-import './js/modal_footer';
-import './js/render_footer_modal';
-import './js/slider_glide';
-
-
-
-
-
-
-
-
-
--- src/js/servises/refs.js
+++ src/js/servises/refs.js
@@ -1,12 +1,5 @@
 const refs = {
   searchForm: document.querySelector('#search-form'),
   gallery: document.querySelector('.gallery-list'),
-
-  footerModal: document.querySelector('.footer-modal'),
-  openModalLink: document.querySelector('[data-footer-open]'),
-  modal: document.querySelector('[data-modal-footer]'),
-  backdrop: document.querySelector('.js-backdrop'),
-  containerSlider: document.querySelector('.container__slider'),
-  sliderTitle: document.querySelector('.slide__title'),
 };
 export default refs;
